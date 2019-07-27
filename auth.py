e21fc052b95e4f31e1fd241777c3c8f4ba14f1fd
Vishal Maurya <vishalmr96@gmail.com>
2019-07-28 01:43:56 +0530

    Updated the permissions

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.appfolder']
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
     """Shows basic usage of the Drive v3 API.
